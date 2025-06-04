cc06423e499e693dcdc031162a6d0dc4a1963dd0
Hayden Welch <91618355+hwelch-fle@users.noreply.github.com>
2025-06-03 23:13:07 -0400

    Refactor | eol_utils.py (#1)
    * Global constants for strformats
    
    * One import per line
    
    * Fix type hint in function header/docstring
    
    * Make sure track has end-of-life key
    
    * Distro info CSV has multiple EOL options
    
    Add literal flag to function header to allow checking for diffrent eols. Update docstring and default to 'eol' for backwards compatibility
    
    * Use Path.open() buffer instead of DictReader
    
    zip headers with values and check for valid base version and valid EOL flag before returning date
    
    Update docstring with second source of value error.
    
    * Use EOL Constants
    
    * remove unused imports
    
    * update function to use implicit string joining
    
    This improves readability and also has a slight performance benefit above a bunch of string adds
    
    * don't allow unnnamed tracks to skip logger warning
    
    * Use unpacking instead of indexing
    
    * Grab version to avoid re import.
    
    This is also more robust as it enfoces a valid base version instead of any (X?X.X?X) string.
    
    * Keep `type | None` hinting consistent and update docstring
    
    * Always check base and move contitional to inline assignment
    
    * invert statments so happy path is first
    
    * add note for early error state with invalid base version.
    
    * remove unused imports
    
    * add comment explaining  `startswith`
    
    * Fix typo in slice
    
    * add .venv to gitignore
    
    * make the f-strings happy
    
    * Remove comment.
    
    Logging message makes it clear that this is for aliased tracks which I guess don't necessarily have valid base version numbers
    
    ---------
    
    Co-authored-by: Hayden Welch <hcw360@gmail.com>

--- src/image/utils/eol_utils.py
+++ src/image/utils/eol_utils.py
@@ -1,62 +1,92 @@
-import csv
-import re
-from datetime import datetime, timezone
+from datetime import (
+    datetime, 
+    timezone,
+)
 from pathlib import Path
-from typing import Any, Optional
+from typing import (
+    Any, 
+    Literal,
+)
 
 from ...shared.logs import get_logger
 
 UBUNTU_DISTRO_INFO = "/usr/share/distro-info/ubuntu.csv"
-VERSION_ID_REGEX = re.compile(r"^(\d{1,2}\.\d{1,2})$")
+
+# Track EOL strfmt
+EOL_TRACK_FMT = "%Y-%m-%dT%H:%M:%SZ"
+
+# Distro Info strfmt
+EOL_DISTRO_FMT = "%Y-%m-%d"
+
+VERSIONS = [
+    version.strip().split(',')[0]
+    for version in Path(UBUNTU_DISTRO_INFO).open(encoding='UTF-8').readlines()[1:]
+]
 
 logger = get_logger()
 
 
-def is_track_eol(track_value: str, track_name: str | None = None) -> bool:
+def is_track_eol(track_value: dict[str, str], track_name: str | None = None) -> bool:
     """Test if track is EOL, or still valid. Log warning if track_name is provided.
 
     Args:
-        track_value (str): The value of the track, a dictionary containing 'end-of-life'.
+        track_value (dict[str, str]): The value of the track, a dictionary containing 'end-of-life' and a iso timestamp.
         track_name (str | None): The name of the track. Defaults to None.
     Returns:
         bool: True if the track is EOL, False otherwise.
     """
-    eol_date = datetime.strptime(
-        track_value["end-of-life"],
-        "%Y-%m-%dT%H:%M:%SZ",
-    ).replace(tzinfo=timezone.utc)
-    is_eol = eol_date < datetime.now(timezone.utc)
+    eol_str = track_value.get('end-of-life')
 
-    if is_eol and track_name is not None:
-        logger.warning(f'Removing EOL track "{track_name}", EOL: {eol_date}')
+    # Handle situations where EOL is not populated, and assumes that it is EOL
+    # This might never happen, but it's a simple check
+    if eol_str is None:
+        logger.error(f"No EOL provided for {track_name or 'UNNAMED TRACK'}! Assuming EOL is now")
+        return True
+    
+    eol_date = datetime.strptime(eol_str, EOL_TRACK_FMT).replace(tzinfo=timezone.utc)
+    is_eol = eol_date < datetime.now(timezone.utc)
+    
+    unknown = 'UNKNOWN TRACK'
+    if is_eol:
+        logger.warning(f'Removing EOL track "{track_name or unknown}", EOL: {eol_date}')
 
     return is_eol
 
 
-def get_base_eol(base: str) -> datetime:
+def get_base_eol(base: str, eol_target: Literal['eol', 'eol-server', 'eol-esm'] = 'eol') -> datetime:
     """Find the EOL of the Ubuntu base image by reading /usr/share/distro-info/ubuntu.csv.
 
     Args:
         base (str): The version ID of the base image, e.g., "22.04".
+        eol_target (Literal['eol', 'eol-server', 'eol-esm']): The EOL target to get (default 'eol')
     Returns:
         datetime: The end-of-life date of the base image.
     Raises:
-        ValueError: If the base image is not found in the CSV file.
+        ValueError: If the base image is not found in the CSV file. Or the slected EOL path is not populated for base image
     """
-    ubuntu_distros = Path(UBUNTU_DISTRO_INFO).read_text(encoding="UTF-8")
-    reader = csv.DictReader(ubuntu_distros.splitlines(), delimiter=",")
-    for row in reader:
-        if row["version"].rstrip("LTS").strip() == base:
-            eol_date = datetime.strptime(
-                row["eol"],
-                "%Y-%m-%d",
-            ).replace(tzinfo=timezone.utc)
-            return eol_date
+    distro_info = Path(UBUNTU_DISTRO_INFO).open(encoding="UTF-8")
+    headers = next(distro_info).strip().split(',') # get headers
 
-    raise ValueError(f"Base image {base} not found in {UBUNTU_DISTRO_INFO}")
+    # Iterate the rows and zip the headers with the valid distros
+    valid_distro: list[dict[str,str]] = [
+        dict(zip(headers, row.strip().split(',')))
+        for row in distro_info
+        if row.startswith(base) # Check that the row starts with base before splitting
+    ]
 
+    # If no valid distros are found, raise ValueError and log error
+    if not valid_distro:
+        raise ValueError(f"Base image {base} not found in {UBUNTU_DISTRO_INFO}")
+    
+    distro = valid_distro.pop(0)
 
-def generate_base_eol_exceed_warning(tracks_eol_exceed_base_eol: list[dict[str, Any]]):
+    if eol_target not in distro:
+        raise ValueError(f"Base image {base} does not have {eol_target}")
+
+    eol_date = datetime.strptime(distro[eol_target], EOL_DISTRO_FMT).replace(tzinfo=timezone.utc)
+    return eol_date
+
+def generate_base_eol_exceed_warning(tracks_eol_exceed_base_eol: list[dict[str, Any]]) -> tuple[str, str]:
     """Generates markdown table for the tracks that exceed the base image EOL date.
 
     Args:
@@ -65,18 +95,26 @@ def generate_base_eol_exceed_warning(tracks_eol_exceed_base_eol: list[dict[str, 
     Returns:
         tuple: A tuple containing the title and text for the warning.
     """
-    title = "Found tracks with EOL date exceeding base image's EOL date\n"
-    text = "Following tracks have an EOL date that exceeds the base image's EOL date:\n"
-    table = "| Track | Base | Track EOL Date | Base EOL Date |\n"
-    table += "|-------|------|----------------|---------------|\n"
-    for build in tracks_eol_exceed_base_eol:
-        table += f"| {build['track']} | {build['base']} | {build['track_eol']} | {build['base_eol']} |\n"
-    text += table
-    text += "\nPlease check the EOL date of the base image and the track.\n"
-    return title, text
+    
+    # Set up lazy record generator
+    table_records = (
+        f"| {build['track']} | {build['base']} | {build['track_eol']} | {build['base_eol']} |" 
+        for build in tracks_eol_exceed_base_eol
+    )
+
+    title = "Found tracks with EOL date exceeding base image's EOL date"
+
+    newline = '\n'
+    # Build body text using implicit string joining
+    body = ("Following tracks have an EOL date that exceeds the base image's EOL date:\n"
+            "| Track | Base | Track EOL Date | Base EOL Date |\n"
+            "|-------|------|----------------|---------------|\n"
+            f"{newline.join(table_records)}" # Insert records
+            "\nPlease check the EOL date of the base image and the track.\n")
+    return title, body
 
 
-def track_eol_exceeds_base_eol(track: str, track_eol: str, base: str | None = None) -> Optional[dict[str, Any]]:
+def track_eol_exceeds_base_eol(track: str, track_eol: str, base: str | None = None) -> dict[str, Any] | None:
     """Check if the track EOL date exceeds the base image EOL date.
 
     Args:
@@ -85,33 +123,28 @@ def track_eol_exceeds_base_eol(track: str, track_eol: str, base: str | None = No
         base (str | None): The base image name, e.g., "ubuntu:22.04". If None, the base will be inferred from the track name.
 
     Returns:
-        Optional[dict[str, Any]]: Dictionary containing the track name, base image, EOL date, and base image EOL date if the track EOL exceeds the base image EOL date.
-        None: If the track EOL date does not exceed the base image EOL date.
+        (dict[str, Any] | None): Dictionary containing the track name, base image, EOL date, and base image EOL date if the track EOL exceeds the base image EOL date.
+
+        (None): If the track EOL date does not exceed the base image EOL date.
     """
-    if not base:
-        base_version_id = track.split("-")[-1]
-        if not VERSION_ID_REGEX.match(base_version_id):
-            logger.warning(f"Track-base-EOL validation skipped for aliased track {track}")
-            return None
-    else:
-        base_version_id = base.split(":")[-1]
+    _, base_version_id = base.split(':') if base is not None else track.split("-")
 
+    # NOTE: str.startswith() is used to avoid rstripping 'LTS' from 'LTS' releases
+    if not any(version.startswith(base_version_id) for version in VERSIONS):
+        logger.warning(f"Track-base-EOL validation skipped for aliased track {track}")
+        return None
+    
     base_eol = get_base_eol(base_version_id)
-    eol_date = datetime.strptime(
-        track_eol,
-        "%Y-%m-%dT%H:%M:%SZ",
-    ).replace(tzinfo=timezone.utc)
+    eol_date = datetime.strptime(track_eol,EOL_TRACK_FMT).replace(tzinfo=timezone.utc)
 
-    if eol_date > base_eol:
-        logger.warning(
-            f"Track {track} has an EOL date {eol_date} that exceeds the base image EOL date {base_eol}"
-        )
+    # Early return for happy path
+    if eol_date < base_eol:
+        return None
 
-        return {
-            "track": track,
-            "base": f"ubuntu:{base_version_id}",
-            "track_eol": eol_date.strftime("%Y-%m-%d"),
-            "base_eol": base_eol.strftime("%Y-%m-%d"),
-        }
-
-    return None
+    logger.warning(f"Track {track} has an EOL date {eol_date} that exceeds the base image EOL date {base_eol}")
+    return {
+        "track": track,
+        "base": f"ubuntu:{base_version_id}",
+        "track_eol": eol_date.strftime(EOL_DISTRO_FMT),
+        "base_eol": base_eol.strftime(EOL_DISTRO_FMT),
+    }
